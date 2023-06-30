bb15a044b21a0cecd6b5380dac43536515107b4d
AnnaRoziii <rozmetovannart@gmail.com>
2023-06-30 14:32:34 +0500

    third commit

--- src/pages/ClubsPage.js
+++ src/pages/ClubsPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import ClubsHero from "../components/clubs/ClubsHero"
 
 const ClubsPage = () => {
   return (
-    <div>
-      <h1>Клубы</h1>
-      <p>Добро пожаловать на страницу клубов! Здесь вы можете создавать и присоединяться к клубам с общими интересами...</p>
-    </div>
+  <>
+  <ClubsHero/>
+  </>
   );
 };
 
