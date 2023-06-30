68bc6aa36f8d3e791f33571caf4d9dc2e13e7708
AnnaRoziii <rozmetovannart@gmail.com>
2023-06-30 07:34:08 +0500

    third commit

--- src/components/tutorsections/TutorDetailPage.jsx
+++ src/components/tutorsections/TutorDetailPage.jsx
@@ -4,11 +4,9 @@ import { useLocation } from 'react-router-dom';
 const TutorDetailPage = () => {
   const location = useLocation();
   const tutor = location.state?.tutor;
-
   if (!tutor) {
     return <div>Tutor not found.</div>;
   }
-
   return (
     <div>
       <h2>{tutor.name}</h2>
